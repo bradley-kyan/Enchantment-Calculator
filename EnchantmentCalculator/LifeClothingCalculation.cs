7dde8350d9221f91f701d17bb14a4744b6a97aaf
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2020-06-01 21:40:45 +1200

    Add project files.

---
+++ EnchantmentCalculator/LifeClothingCalculation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnchantmentCalculator
+{
+    class lifeCP1
+    {
+        public double fs_amount { get; set; }
+        public static readonly lifeCP1 Instance = new lifeCP1();
+        public static double Percentage(double fs_amount)
+        {
+            if (fs_amount <= 14)
+            {
+                return (fs_amount * 3) + 30;
+            }
+            else
+            {
+                return ((fs_amount - 14) * .6) + 72;
+            }
+        }
+
+    }
+    class lifeCP2
+    {
+        public double fs_amount { get; set; }
+        public static readonly lifeCP2 Instance = new lifeCP2();
+        public static double Percentage(double fs_amount)
+        {
+            if (fs_amount <= 40)
+            {
+                return fs_amount + 10;
+            }
+            else
+            {
+                return ((fs_amount - 40) * .2) + 50;
+            }
+        }
+
+    }
+    class lifeCP3
+    {
+        public double fs_amount { get; set; }
+        public static readonly lifeCP3 Instance = new lifeCP3();
+        public static double Percentage(double fs_amount)
+        {
+            if (fs_amount <= 44)
+            {
+                return (fs_amount * .75) + 7.5;
+            }
+            else
+            {
+                return ((fs_amount - 44) * .15) + 40.5;
+            }
+        }
+
+    }
+    class lifeCP4
+    {
+        public double fs_amount { get; set; }
+        public static readonly lifeCP4 Instance = new lifeCP4();
+        public static double Percentage(double fs_amount)
+        {
+            if (fs_amount <= 110)
+            {
+                return (fs_amount * .25) + 2.5;
+            }
+            else
+            {
+                return ((fs_amount - 110) * .05) + 30;
+            }
+        }
+
+    }
+    class lifeCP5
+    {
+        public double fs_amount { get; set; }
+        public static readonly lifeCP5 Instance = new lifeCP5();
+        public static double Percentage(double fs_amount)
+        {
+            return (fs_amount * .05) + .5;
+        }
+
+    }
+}
---
+++ EnchantmentCalculator/Program.cs
@@ -0,0 +1,86 @@
+﻿using EnchantmentCalculator;
+using System;
+using System.Dynamic;
+
+namespace EnchantmentCalculator
+{
+    class Header
+    {
+        public static readonly Header header = new Header();
+        public static string Title()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ________________________ ");
+            Console.WriteLine("|                        |");
+            Console.WriteLine("| Enchantment Calculator |");
+            Console.WriteLine("|________________________|");
+            Console.ResetColor();
+            return "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lifeClothing();
+        }
+
+        static void lifeClothing()
+        {
+            try
+            {
+                lifeCP1 PecentageCalc = new lifeCP1();
+                double equipment_Base_price;
+                double repair_item_cost;
+                string correct = "n";
+
+                Console.Clear();
+                Console.WriteLine(Header.Title());
+                Console.WriteLine("Enter Life Clothing Upgrade (1, 2, 3, 4, 5)");
+                double Upgrade = Convert.ToInt64(Console.ReadLine());
+                Console.Clear();
+
+                do
+                {
+
+                    Console.WriteLine(Header.Title());
+                    Console.WriteLine("Enter Desired Failstacks");
+                    lifeCP1.Instance.fs_amount = Convert.ToInt64(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine(Header.Title());
+                    Console.WriteLine("Enter Repair Item Cost");
+                    repair_item_cost = Convert.ToInt64(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine(Header.Title());
+                    Console.WriteLine("Enter Enchantment Item Cost");
+                    equipment_Base_price = Convert.ToInt64(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine(Header.Title());
+                    Console.WriteLine("Desired Failstacks: " + lifeCP1.Instance.fs_amount);
+                    Console.WriteLine("Failstacks Percentage : " + lifeCP1.Percentage(lifeCP1.Instance.fs_amount));
+                    Console.WriteLine("'Repair Item' Cost : " + repair_item_cost);
+                    Console.WriteLine("'Enchant Item' Cost : " + repair_item_cost);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Is This Correct? (Y/N)");
+                    correct = Convert.ToString(Console.ReadLine());
+
+                    Console.Clear();
+
+                }
+                while (correct == "n");
+            }
+            catch
+            {
+                lifeClothing();
+            }
+
+
+        }
+    }
+}
+
+
